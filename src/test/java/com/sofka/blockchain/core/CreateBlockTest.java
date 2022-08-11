32a1e26f6d30fa0134b1b9ff9a120d1681854d13
Julian Lasso <98534497+ProfeJulianLasso@users.noreply.github.com>
2022-08-11 14:03:06 -0500

    Test creado en la charla

---
+++ src/test/java/com/sofka/blockchain/core/CreateBlockTest.java
@@ -0,0 +1,11 @@
+package com.sofka.blockchain.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CreateBlockTest {
+
+    @Test
+    void generateBlock() {
+    }
+}
